04d5347d75cecd1498025eb518022ce443b6a3ca
Spb-Webmaster <spb-webmaster@mail.ru>
2024-07-08 16:06:56 +0300

    08jule

--- resources/views/pages/catalog/object/object.blade.php
+++ resources/views/pages/catalog/object/object.blade.php
@@ -16,43 +16,116 @@
             @include('pages.catalog.object.partial._object_logo')
 
 
-           @include('include.menu.object_menu')
+            @include('include.menu.object_menu')
 
             <div class="ob_main_pageHtml ob_main_page block block_1123 ">
-<div class="block">
-                <div class="page_l">
-                <div class="page_l__left">
-                    @if($item->main_title)
-                        <h2 class="_h2">
-                            {{ $item->main_title  }}
-                        </h2>
-                    @endif
-                    <div class="desc desc_main__content">
-                        {!! $item->main_desc !!}
+                <div class="block">
+                    <div class="page_l">
+                        <div class="page_l__left">
+                            @if($item->main_title)
+                                <h2 class="_h2">
+                                    {{ $item->main_title  }}
+                                </h2>
+                            @endif
+                            <div class="desc desc_main__content">
+                                {!! $item->main_desc !!}
+                            </div>
+
+                        </div>
+                        <div class="page_l__right">
+
+                            @if($item->main_right_img )
+                                <div class="desc desc_main__imgR pad_t33">
+                                    <img class="pc_category_img" width="228" height="270" loading="lazy"
+                                         src="{{ asset(intervention('228x270', $item->main_right_img, 'objects')) }}"
+                                         alt="{{$item->main_right_img}}">
+                                </div>
+                                @if($item->main_right_img_text )
+                                    <div class="desc desc_main__signature_img">
+                                        {!! $item->main_right_img_text !!}
+                                    </div>
+                                @endif
+                            @endif
+
+
+                        </div>
                     </div>
 
-                </div>
-                <div class="page_l__right">
 
-                    @if($item->main_right_img )
-                        <div class="desc desc_main__imgR pad_t33">
-                            <img class="pc_category_img" width="228" height="270" loading="lazy"
-                                 src="{{ asset(intervention('228x270', $item->main_right_img, 'objects')) }}"
-                                 alt="{{$item->main_right_img}}">
-                        </div>
-                        @if($item->main_right_img_text )
-                            <div class="desc desc_main__signature_img">
-                                {!! $item->main_right_img_text !!}
+                    @if($item->a_img )
+                        <div class="page_l page_l__revers">
+
+                            <div class="page_l__left">
+                                @if($item->a_img )
+                                    <div class="desc desc_main__imgR pad_t33">
+                                        <img class="pc_category_img" width="228" height="270" loading="lazy"
+                                             src="{{ asset(intervention('228x270', $item->a_img, 'objects')) }}"
+                                             alt="{{$item->a_img}}">
+                                    </div>
+                                @endif
                             </div>
-                        @endif
+
+                            <div class="page_l__right">
+                                <div class="desc desc_main__content">
+                                    {!! $item->a_desc !!}
+                                </div>
+                            </div>
+
+                        </div>
+                    @endif
+
+                    @if($item->a_desc2)
+                        <div class="desc a_desc2 pad_t20">{!! $item->a_desc2 !!}</div>
+                    @endif
+
+                    @if($item->a_img2)
+                        <div class="desc a_desc2 pad_t20">
+                            <img class="pc_category_img" width="100%" style="width: 100%; height: auto" loading="lazy"
+                                                                     src="{{ asset(Storage::disk('public')->url($item->a_img2)) }}"
+                                                                     alt="photo" />
+                        </div>
+                    @endif
+
+                    @if($item->a_img3)
+                    <div class="page_l pad_t26_important no_back">
+
+                        <div class="page_l__left">
+                            <div class="desc desc_main__content">
+                                {!! $item->a_desc3 !!}
+                            </div>
+                        </div>
+
+                        <div class="page_l__right">
+                            @if($item->main_right_img )
+                                <div class="desc desc_main__imgR pad_t33">
+                                    <img class="pc_category_img" width="228" height="270" loading="lazy"
+                                         src="{{ asset(intervention('228x270', $item->a_img3, 'objects')) }}"
+                                         alt="{{$item->a_img3}}">
+                                </div>
+                            @endif
+                        </div>
+
+                    </div>
+                    @endif
+
+                    @if($item->a_desc4)
+                        <div class="desc a_desc2 pad_t26">{!! $item->a_desc4 !!}</div>
+                    @endif
+
+                    @if($item->a_img3)
+                        <div class="desc a_desc2 pad_t20">
+                            <img class="pc_category_img" width="100%" style="width: 100%; height: auto" loading="lazy"
+                                 src="{{ asset(Storage::disk('public')->url($item->a_img3)) }}"
+                                 alt="photo" />
+                        </div>
                     @endif
 
 
 
 
                 </div>
-                </div>
-</div>
+
+
             </div>
 
         </div>
